ce54bad72bb8a5697e8bfc0326329cbe24f4465d
martinteoharov <martin.teoharov15@gmail.com>
2021-01-26 14:03:00 +0000

    Refactor; Add colors.js for better logging

--- index.js
+++ index.js
@@ -35,8 +35,6 @@ const typeDefs = gql(`
     }
 
     type Query {
-        getAllUsers: [User]
-
         getAllTrackers: [Tracker]
     }
 
@@ -50,11 +48,6 @@ const typeDefs = gql(`
 
 const resolvers = {
     Query: {
-        /* USER */
-        getAllUsers: async (parent, args, context) => {
-            return await context.models.User.getAllUsers();
-        },
-
         /* Get all the trackers for a specific user.. */
         getAllTrackers: async (parent, args, context) => {
             const trackers = await context.models.Tracker.getAllTrackers({ 'username': context.user.username });
@@ -63,19 +56,13 @@ const resolvers = {
         },
     },
     Mutation: {
-
-        /* AUTH */
         login: async (parent, args, context) => {
             // Find user in DB..
             const user = await context.models.User.getUserByUsername({ username: args.username });
-
             if(!user) return;
-
             if(user.password === args.password){
-                
                 // Generate JWT...
                 const token = jwt.sign(user, jwtSecret);
-
                 return token;
             }
             // TODO: Send error codes..
@@ -83,18 +70,14 @@ const resolvers = {
         },
         register: async (parent, args, context) => {
             const user = { username: args.username, password: args.password };
-
             // Add user to DB...
             const res = await context.models.User.addUser(user);
-
             // Generate JWT...
             const token = jwt.sign(user, jwtSecret);
-
             // TODO: Send error codes..
             return token || "Something failed, it's probably your fault dummy";
         },
 
-        /* TRACKER */
         addTracker: async (parent, args, context) => {
             const tracker = await context.models.Tracker.addTracker({ username: context.user.username, name: args.name, startDate: args.startDate, endDate: args.endDate, timer: args.timer });
             return tracker;
@@ -107,39 +90,40 @@ const resolvers = {
 }
 
 const server = new ApolloServer({
-    // GraphQL type defs
-    typeDefs,
-
-    // Resolvers
-    resolvers,
-
+    typeDefs, // GraphQL type defs
+    resolvers, // Resolvers
     /*
      * The context object is one that gets passed to every single resolver at every level, so we 
      * can access it anywhere in our schema code. It’s where we can store things like data fetchers, 
      * database connections, and (conveniently) information about the user making the request
      * 
-     * Think of context as middleware ran before every resolver exec
+     * Additionally we can think of context as middleware ran before every resolver exec, so we do the
+     * auth here
     */
     context: ({ req }) => {
-        const token = req.headers.authorization.split(' ')[1].slice(0, -1);
+        console.log(`REQUEST: Req Referer: ${ req.headers.referer }`);
+        console.log(`REQUEST: Req Operation: ${ req.body.operationName }`);
 
-        // Try to get the user from the token...
-        let user;
-        try {
-            user = jwt.verify(token, jwtSecret);
-            console.log(`INFO: Decoded User: ${user}`);
-        }
-        catch (e){
-            console.log(`ERROR: ${e}`);
+        let user, token;
+        const models = { models: { User, Tracker }};
+
+        if(req.body.operationName === 'Login' || req.body.operationName === 'Register'){
+            //console.log(`Return ${JSON.stringify({ ...models, user: null })}`);
+            return { ...models, user: null };
         }
 
-        return { 
-            user,
-            models: {
-                User: User,
-                Tracker: Tracker,
+        if(req.headers.authorization){
+            token = req.headers.authorization.split(' ')[1];
+            try {
+                user = jwt.verify(token, jwtSecret);
+                console.log(`INFO: Decoded User: ${ JSON.stringify(user) }`);
+                return { ...models, user:user };
             }
-        };
+            catch (e){
+                console.log(`ERROR: ${ e }`);
+                return null;
+            }
+        }
     },
 });
 
--- models/User.js
+++ models/User.js
@@ -1,8 +1,5 @@
 const mongo = require('../db/mongoUtil');
 
-const getAllUsers = async () => {
-    return await mongo.getDB().collection('users').find({}, { username: 1, password: 0 }).toArray();
-}
 const getUserByUsername = async ({ username }) => {
     const users = await mongo.getDB().collection('users').find({ username }).toArray();
     return users[0];
@@ -11,6 +8,5 @@ const addUser = async ({ username, password }) => {
     return mongo.getDB().collection('users').insertOne({ username, password });
 }
 
-module.exports.getAllUsers = getAllUsers;
 module.exports.getUserByUsername = getUserByUsername;
 module.exports.addUser = addUser;
